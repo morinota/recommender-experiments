417df0dfe33fe504bd9b5683afb09b08580a7aad
morita <morinota416@gmail.com>
2025-01-13 15:53:19 +0900

    feat: シミュレーション関数を追加

--- src/recommender_experiments/app/cost_benefit_experiment/__main__.py
+++ src/recommender_experiments/app/cost_benefit_experiment/__main__.py
@@ -1,3 +1,5 @@
+import itertools
+from pathlib import Path
 import random
 from typing import Callable, Literal, Optional, TypedDict
 import numpy as np
@@ -134,8 +136,15 @@ def _run_single_simulation(
     )
     print(f"policy_value_before_fit: {policy_value_before_fit}")
 
+    # 新方策の性能の推移を保存するlist
+    new_policy_value_list: list[tuple] = []
+
+    # 学習データ数を10分割して、段階的にfittingを行い、新方策の性能の推移を記録する
+    splitted_n_rounds_train = n_rounds_train // 10
     for i in range(10):
-        bandit_feedback_train = dataset.obtain_batch_bandit_feedback(n_rounds_train)
+        bandit_feedback_train = dataset.obtain_batch_bandit_feedback(
+            splitted_n_rounds_train
+        )
         # データ収集方策で集めたデータ(学習用)で、新方策のためのNNモデルのパラメータを更新
         if new_policy_setting == "obp_nn":
             new_policy.fit(
@@ -168,35 +177,105 @@ def _run_single_simulation(
             action_dist=test_action_dist,
         )
         print(
-            f"ground_truth_new_policy_value(at {i * n_rounds_train} data): {ground_truth_new_policy_value}"
+            f"ground_truth_new_policy_value(at {(i+1) * splitted_n_rounds_train} data): {ground_truth_new_policy_value}"
+        )
+        new_policy_value_list.append(
+            ((i + 1) * splitted_n_rounds_train, ground_truth_new_policy_value)
         )
 
     return {
         "simulation_idx": simulate_idx,
+        "n_actions": n_actions,
         "n_rounds_train": n_rounds_train,
         "n_rounds_test": n_rounds_test,
-        "n_actions": n_actions,
         "expected_reward_lower": expected_reward_lower,
         "expected_reward_upper": expected_reward_upper,
         "ground_truth_new_policy_value": ground_truth_new_policy_value,
+        "new_policy_value_list": new_policy_value_list,
     }
 
 
+from loguru import logger
+from pandera.polars import DataFrameSchema
+from pandera.typing.polars import DataFrame
+
+
+class SimulationResult(DataFrameSchema):
+    n_actions: int
+    n_rounds_train: int
+    n_rounds_test: int
+    expected_reward_lower: float
+    expected_reward_upper: float
+    expected_reward_setting: Literal["my_context_free", "my_context_aware", "linear"]
+    new_policy_setting: Literal["two_tower_nn", "obp_nn"]
+    ground_truth_new_policy_value: float
+
+
 def _run_multiple_simulations(
-    n_rounds_train_list: list[int],
-    n_rounds_test_list: list[int],
-    n_actions_list: list[int],
-    dim_context_list: list[int],
-    expected_reward_scale_list: list[tuple[float, float]],
-) -> pl.DataFrame:
-    pass
+    n_actions_list: list[int] = [5, 10],
+    dim_context_list: list[int] = [5, 10],
+    expected_reward_scale_list: list[tuple[float, float]] = [
+        (0.1, 0.2),
+        (0.1, 0.5),
+    ],
+) -> DataFrame[SimulationResult]:
+    # 固定値を定義
+    fixed_n_round_train = 1000
+    fixed_n_round_test = 1000
+
+    # シミュレーション設定の組み合わせを作成
+    simulate_configs = list(
+        itertools.product(
+            n_actions_list,
+            dim_context_list,
+            expected_reward_scale_list,
+        )
+    )
+    print(f"simulate_configs: {simulate_configs}")
+
+    results = []
+    for config in simulate_configs:
+        n_actions, dim_context, (expected_reward_lower, expected_reward_upper) = config
+
+        # 期待報酬関数の設定の都合で、action_contextの次元数をdim_contextと同じにしているが、別に必須ではない。
+        action_context = np.random.random((n_actions, dim_context))
+        result_dict = _run_single_simulation(
+            simulate_idx=0,
+            n_rounds_train=fixed_n_round_train,
+            n_rounds_test=fixed_n_round_test,
+            n_actions=n_actions,
+            dim_context=dim_context,
+            action_context=action_context,
+            logging_policy_function=_logging_policy,
+            expected_reward_lower=expected_reward_lower,
+            expected_reward_upper=expected_reward_upper,
+            expected_reward_setting="my_context_aware",
+            new_policy_setting="two_tower_nn",
+        )
+
+        # result_dictのnew_policy_value_listを分解して、dfの1行にする
+        for n_round_train, new_policy_value in result_dict["new_policy_value_list"]:
+            results.append(
+                {
+                    "n_actions": n_actions,
+                    "n_rounds_train": fixed_n_round_train,
+                    "n_rounds_test": fixed_n_round_test,
+                    "expected_reward_lower": expected_reward_lower,
+                    "expected_reward_upper": expected_reward_upper,
+                    "expected_reward_setting": "my_context_aware",
+                    "new_policy_setting": "two_tower_nn",
+                    "n_round_train": n_round_train,
+                    "new_policy_value": new_policy_value,
+                }
+            )
+
+    return pl.DataFrame(results)
 
 
 def main() -> None:
     # 実験パラメータ
     n_actions_list = [5, 10, 20, 50, 100, 1000]
     dim_context_list = [5, 10, 20, 50, 100]
-    n_rounds_train = list(range(1000, 100000, 2000))
     expected_reward_scale_list = [
         (0.1, 0.9),
         (0.1, 0.7),
@@ -204,22 +283,29 @@ def main() -> None:
         (0.1, 0.3),
         (0.1, 0.2),
     ]
-    print(
-        _run_single_simulation(
-            simulate_idx=0,
-            n_rounds_train=1000,
-            n_rounds_test=1000,
-            n_actions=4,
-            dim_context=50,
-            action_context=np.random.random((4, 50)),
-            logging_policy_function=_logging_policy,
-            expected_reward_lower=0.05,
-            expected_reward_upper=0.10,
-            expected_reward_setting="my_context_aware",
-            # new_policy_setting="obp_nn",
-            new_policy_setting="two_tower_nn",
-        )
-    )
+    result_df = _run_multiple_simulations()
+
+    # 結果の保存
+    results_dir = Path("logs/cost_benefit_experiment/")
+    results_dir.mkdir(parents=True, exist_ok=True)
+    result_df.write_csv(results_dir / "result_df.csv")
+
+    # print(
+    #     _run_single_simulation(
+    #         simulate_idx=0,
+    #         n_rounds_train=10000,
+    #         n_rounds_test=1000,
+    #         n_actions=4,
+    #         dim_context=50,
+    #         action_context=np.random.random((4, 50)),
+    #         logging_policy_function=_logging_policy,
+    #         expected_reward_lower=0.05,
+    #         expected_reward_upper=0.10,
+    #         expected_reward_setting="my_context_aware",
+    #         # new_policy_setting="obp_nn",
+    #         new_policy_setting="two_tower_nn",
+    #     )
+    # )
 
 
 if __name__ == "__main__":

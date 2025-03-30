d4f73c0cfb520a2b56c4655994c1c3ac864548f5
morita <morinota416@gmail.com>
2025-03-30 19:20:00 +0900

    feat: EnvironmentStrategyInterfaceに真の方策の価値を計算するメソッドを追加し、create_bandit_feedbackメソッドの名前を変更

--- src/recommender_experiments/service/environment/environment_strategy_interface.py
+++ src/recommender_experiments/service/environment/environment_strategy_interface.py
@@ -13,7 +13,7 @@ class EnvironmentStrategyInterface(abc.ABC):
     """
 
     @abc.abstractmethod
-    def create_bandit_feedback(
+    def obtain_batch_bandit_feedback(
         self,
         logging_policy_strategy: PolicyStrategyInterface,
         n_rounds: int,
@@ -22,5 +22,20 @@ class EnvironmentStrategyInterface(abc.ABC):
         Args:
             logging_policy_strategy (PolicyStrategyInterface): データ収集方策のstrategy
             n_rounds (int): ラウンド数
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def calc_ground_truth_policy_value(
+        self,
+        expected_reward: np.ndarray,
+        action_dist: np.ndarray,
+    ) -> float:
+        """真の方策の価値を計算するメソッド
+        Args:
+            expected_reward (np.ndarray): 期待報酬関数 q(x,a) = E_{p(r|x,a)}[r] の配列 (n_rounds, n_actions)
+            action_dist (np.ndarray): 任意の方策 \pi による行動選択確率分布の配列 (n_rounds, n_actions)
+        Returns:
+            float: 方策性能 V_{\pi}
         """
         raise NotImplementedError

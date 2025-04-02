cfd314cc4caed6c4a9bbc672fb80390ffa59b0d6
morita <morinota416@gmail.com>
2025-04-02 21:56:39 +0900

    feat: ContextualBanditPolicyクラスのテストと実装を追加

--- src/recommender_experiments/service/opl/contexual_bandit_policy.py
+++ src/recommender_experiments/service/opl/contexual_bandit_policy.py
@@ -1,22 +1,73 @@
 from typing import Optional
-from obp.policy import BernoulliTS
+from obp.policy import LogisticTS
 import numpy as np
-from recommender_experiments.service.opl.policy_strategy_interface import PolicyStrategyInterface
-from recommender_experiments.service.synthetic_bandit_feedback import BanditFeedbackDict
+from recommender_experiments.service.opl.policy_strategy_interface import (
+    PolicyStrategyInterface,
+)
+from recommender_experiments.service.synthetic_bandit_feedback import (
+    BanditFeedbackDict,
+    BanditFeedbackModel,
+)
 
 
 class ContextualBanditPolicy(PolicyStrategyInterface):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        n_actions: int,
+        dim_context: int,
+        temperature: float = 1.0,
+    ) -> None:
+
+        self.__model = LogisticTS(
+            dim=dim_context,
+            n_actions=n_actions,
+            len_list=1,
+            batch_size=1,
+            random_state=None,
+        )
+        self.__temperature = temperature
 
     @property
     def policy_name(self) -> str:
         return "ContextualBanditPolicy"
 
     def fit(
-        self, bandit_feedback_train: BanditFeedbackDict, bandit_feedback_test: Optional[BanditFeedbackDict] = None
+        self,
+        bandit_feedback_train: BanditFeedbackModel,
+        bandit_feedback_test: Optional[BanditFeedbackModel] = None,
     ) -> None:
-        pass
 
-    def predict_proba(self, context: np.ndarray, action_context: np.ndarray, random_state: int = 0) -> np.ndarray:
-        return np.zeros(action_context.shape[0])
+        contexts = list(bandit_feedback_train.context)
+        actions = list(bandit_feedback_train.action)
+        rewards = list(bandit_feedback_train.reward)
+
+        for x, a, r in zip(contexts, actions, rewards):
+            self.__model.update_params(
+                # contextのshapeは(1, dim_context)にする必要あり
+                context=x.reshape(1, -1),
+                action=a,
+                reward=r,
+            )
+
+    def predict_proba(
+        self, context: np.ndarray, action_context: np.ndarray, random_state: int = 0
+    ) -> np.ndarray:
+        n_rounds = context.shape[0]
+        n_actions = action_context.shape[0]
+        action_dist = np.zeros((n_rounds, n_actions))
+
+        # obp.policy.LogisticTSクラスの実装の都合上、ミニバッチで推論させてる。
+        # (まあ推論時間の遅さは実験なので許容してる。実務で使う時は要検討)
+        for i in range(n_rounds):
+            x = context[i].reshape(1, -1)
+            # パラメータの期待値を使って算出された報酬期待値の一覧を取得
+            theta = np.array(
+                [model.predict_proba(x) for model in self.__model.model_list]
+            ).flatten()
+            print(f"theta: {theta}")
+
+            # ソフトマックス関数に通して行動選択確率分布を算出
+            action_dist[i] = np.exp(theta / self.__temperature) / np.sum(
+                np.exp(theta / self.__temperature)
+            )
+        return action_dist
--- tests/service/opl/test_contexual_bandit_policy.py
+++ tests/service/opl/test_contexual_bandit_policy.py
@@ -1,7 +1,53 @@
-from recommender_experiments.service.opl.contexual_bandit_policy import ContextualBanditPolicy
+import numpy as np
+from recommender_experiments.service.opl.contexual_bandit_policy import (
+    ContextualBanditPolicy,
+)
 from obp.policy import BernoulliTS
 
+from recommender_experiments.service.synthetic_bandit_feedback import (
+    BanditFeedbackModel,
+)
 
-def test_ContexualBanditモデルが正しく初期化されること():
-    sut = ContextualBanditPolicy()
-    assert True
+
+def test_ContexualBanditモデルがbandit_feedbackを元に学習されること():
+    # Arrange
+    n_actions = 4
+    dim_context = 10
+    sut = ContextualBanditPolicy(n_actions=n_actions, dim_context=dim_context)
+    bandit_feedback_train = BanditFeedbackModel(
+        n_rounds=100,
+        n_actions=n_actions,
+        context=np.random.random((100, dim_context)),
+        action_context=np.random.random((n_actions, dim_context)),
+        action=np.random.randint(0, n_actions, 100),
+        position=None,
+        reward=np.random.binomial(1, 0.5, 100),
+        expected_reward=np.random.random((100, n_actions)),
+        pi_b=np.random.random((100, n_actions)),
+        pscore=np.random.random(100),
+    )
+
+    # Act
+    sut.fit(bandit_feedback_train)
+
+    # Assert
+    assert sut.policy_name == "ContextualBanditPolicy"
+
+
+def test_ContexualBanditモデルがあるcontextに対する行動選択確率分布を出力できること():
+    # Arrange
+    n_actions = 4
+    dim_context = 10
+    sut = ContextualBanditPolicy(n_actions=n_actions, dim_context=dim_context)
+    context = np.random.random((5, dim_context))
+    action_context = np.random.random((n_actions, dim_context))
+
+    # Act
+    action_dist = sut.predict_proba(context, action_context)
+
+    # Assert
+    assert isinstance(action_dist, np.ndarray), "行動選択確率分布はndarrayである"
+    assert action_dist.shape == (
+        5,
+        n_actions,
+    ), "行動選択確率分布のshapeは(ラウンド数, アクション数)である"

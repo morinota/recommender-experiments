cfe4f633a1b0fbb547303de96a32cede8383c859
morita <morinota416@gmail.com>
2025-04-16 22:09:06 +0900

    feat: PolicyStrategyInterfaceのfitおよびpredict_probaメソッドのシグネチャを整形し、sampleメソッドを追加

--- src/recommender_experiments/service/opl/policy_strategy_interface.py
+++ src/recommender_experiments/service/opl/policy_strategy_interface.py
@@ -12,17 +12,13 @@ class PolicyStrategyInterface(abc.ABC):
     """
 
     @abc.abstractmethod
-    def fit(
-        self, bandit_feedback_train: dict, bandit_feedback_test: Optional[dict] = None
-    ) -> None:
+    def fit(self, bandit_feedback_train: dict, bandit_feedback_test: Optional[dict] = None) -> None:
         """bandit feedbackを受け取って方策を更新するメソッド"""
         # TODO: シグネチャをdictではなくBanditFeedbackModelにするか要検討...!
         raise NotImplementedError
 
     @abc.abstractmethod
-    def predict_proba(
-        self, context: np.ndarray, action_context: np.ndarray, random_state: int = 0
-    ) -> np.ndarray:
+    def predict_proba(self, context: np.ndarray, action_context: np.ndarray, random_state: int = 0) -> np.ndarray:
         """contextを受け取って、方策による行動選択確率 \pi(a|x) を予測するメソッド
         Args:
             context (np.ndarray): コンテキスト特徴量の配列 (n_rounds, dim_context_features)
@@ -32,6 +28,22 @@ class PolicyStrategyInterface(abc.ABC):
             np.ndarray: 行動選択確率 \pi_{\theta}(a|x) の配列 (n_rounds, n_actions, 1)
         """
         raise NotImplementedError
+
+    @abc.abstractmethod
+    def sample(
+        self, context: np.ndarray, action_context: np.ndarray, random_state: int = 0
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """contextを受け取って、方策による行動選択確率 \pi(a|x) に従ってactionをサンプリングするメソッド
+        Args:
+            context (np.ndarray): コンテキスト特徴量の配列 (n_rounds, dim_context_features)
+            action_context (np.ndarray): アクション特徴量の配列 (n_actions, dim_action_features)
+            random_state (int): 乱数シード
+        Returns:
+            tuple[np.ndarray, np.ndarray]:
+            1つ目が(n_rounds, )の選択されたactionのindex配列、
+            2つ目が(n_rounds, )の選択されたactionの傾向スコア配列。
+        """
+        pass  # 実装してなくてもエラーにはしない。
 
     @property
     @abc.abstractmethod
